2b49fb6745c8d0aba04c7de52560f1fd94f64767
sinchan143054 <sinchan.143054@gmail.com>
2025-06-09 10:09:01 +0530

    save

---
+++ number.java
@@ -0,0 +1,10 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+public class number {
+    public static void main(String[] var0) {
+      int num = 10;
+      if (num> 0) {
+         System.out.println("Number is positive");
+      }
+
+   }
+}
